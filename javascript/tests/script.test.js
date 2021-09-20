30e046c5031adac64eea2dac13271fd80e20d301
dwelguisz <dwelguisz@indeed.com>
2021-09-20 18:10:01 -0500

    Fails for length greater than 8

--- javascript/tests/script.test.js
+++ javascript/tests/script.test.js
@@ -6,3 +6,7 @@ const check_length = require('../script')
 test('if length is less than 8, expect false to be returned', () => {
     expect(check_length(3)).toBe(false);
 })
+
+test('if length is greater than or equal to 8 and less than or equal to 128, expect true to be returned', () => {
+    expect(check_length(12)).toBe(true);
+})
