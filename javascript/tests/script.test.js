6f0f011d39afd5b810b59084d0717874bc431fba
dwelguisz <dwelguisz@indeed.com>
2021-09-20 18:11:57 -0500

    Fail for length greater than 128

--- javascript/tests/script.test.js
+++ javascript/tests/script.test.js
@@ -10,3 +10,7 @@ test('if length is less than 8, expect false to be returned', () => {
 test('if length is greater than or equal to 8 and less than or equal to 128, expect true to be returned', () => {
     expect(check_length(12)).toBe(true);
 })
+
+test('if length is greater than 128, expect falsed to be returned', () => {
+    expect(check_length(129)).toBe(false));
+})
