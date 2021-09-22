05780553accb71a57fa76eec7e7e8bcc3c4b2af3
dwelguisz <dwelguisz@indeed.com>
2021-09-21 21:08:35 -0500

    Tests to check the create_char_array works

--- javascript/tests/script.test.js
+++ javascript/tests/script.test.js
@@ -36,5 +36,36 @@ describe('THEN my input should be validated and at least one character type shou
 })
 
 describe('Check that different arrays are given when different character sets are selected', () => {
-
+    test('if uppercase selected, then char array of uppercase is returned', () => {
+        const val = password_generator.create_char_array(true, false, false, false);
+        expect(val.length).toBe(26);
+        expect(val).toStrictEqual(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+        "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]);
+    })
+    test('if lowercase selected, then char array of lowercase is returned', () => {
+        const val = password_generator.create_char_array(false, true, false, false);
+        expect(val.length).toBe(26);
+        expect(val).toStrictEqual(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+    })
+    test('if numeric selected, then char array of numbers is returned', () => {
+        const val = password_generator.create_char_array(false, false, true, false);
+        expect(val.length).toBe(10);
+        expect(val).toStrictEqual(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+    })
+    test('if special is selected, then char array of special characters is returned', () => {
+        const val = password_generator.create_char_array(false, false, false, true);
+        expect(val.length).toBe(33);
+        expect(val).toStrictEqual([" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")",
+            "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[",
+            "\\", "]", "^", "_", "`", "{", "|", "}", "~"]);
+    })
+    test('check that different options return different sizes', () => {
+        expect(password_generator.create_char_array(true, true, false, false).length).toBe(52);
+        expect(password_generator.create_char_array(true, false, true, false).length).toBe(36);
+        expect(password_generator.create_char_array(true, false, false, true).length).toBe(59);
+        expect(password_generator.create_char_array(true, true, true, false).length).toBe(62);
+        expect(password_generator.create_char_array(true, true, false, true).length).toBe(85);
+        expect(password_generator.create_char_array(true, true, true, true).length).toBe(95);
+    })
 })
